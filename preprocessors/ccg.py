da885fc6385ae8a422e88f1bb9e61870e6e23ac5
Brian Yu <brianyu28@gmail.com>
2018-11-27 10:59:31 -0500

    Updated CCG preprocessor

--- preprocessors/ccg.py
+++ preprocessors/ccg.py
@@ -7,6 +7,16 @@ from helpers import replace_smart
 SERVER = "http://127.0.0.1:5000/ccg"
 
 def preprocess(contents):
+    """For preprocessing a single sentence."""
+    tags = requests.post(SERVER, data={"data": contents}, timeout=10)
+    if tags.status_code != 200:
+        return "NONE"
+    else:
+        return tags.text
+
+
+def preprocess_many(contents):
+    """For preprocessing many sentences."""
     sentences = nltk.sent_tokenize(replace_smart(contents))
     all_tags = []
     for sentence in sentences:
